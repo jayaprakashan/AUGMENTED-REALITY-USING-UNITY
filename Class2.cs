7684e51d6cf451e33e36c1f2945d15d96a9630d2
jayaprakashan <31839536+jayaprakashan@users.noreply.github.com>
2019-08-27 13:27:43 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Class2 : MonoBehaviour
+{
+
+      void Start()
+        {
+            Label l2 = new Label();
+            l2.Text = "This is a new Label";
+            l2.BorderStyle = BorderStyle.Solid;
+            Page.Controls.Add(l2);
+        }
+       
+    
+}
